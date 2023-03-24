94a7ddbb2e2afb2d58e2720b44185593046ad451
JakubZegota <s24317@pjwstk.edu.pl>
2023-03-24 14:27:54 +0100

    gallery scalability, previous/next btns

--- index.js
+++ index.js
@@ -1,38 +1,120 @@
+imagesArray = [
+  img1 = { imgSource: "mosscloseup.jpg", description: "Moss in close-up" },
+  img2 = { imgSource: "mosscloseup2.jpg", description: "Anther picture of moss in close-up" },
+  img3 = { imgSource: "mossgarden.jpg", description: "Moss garden" },
+  img4 = { imgSource: "mossgarden2.jpg", description: "Another moss garden" },
+];
 
-function showfullpicture(imagejpg, imageDescription) {
+const galleryContainer = document.getElementById("galery-container");
 
-    imageUrl = "Gallery/"+imagejpg;
+let i = 0;
+for (const image of imagesArray) {
+  const imageElement = new Image();
+  imageElement.src = "Gallery/" + image.imgSource;
+  imageElement.alt = "image=" + image.imgSource;
+  imageElement.classList.add("gallery-img"); 
+  imageElement.addEventListener('click', function(){  
+    showfullpicture(image);
+  });
 
-    const fullscreenImage = document.createElement('div');
-    fullscreenImage.classList.add('full-screen-image');
-
-
-    const imageDescriptionContainer = document.createElement('div');
-    imageDescriptionContainer.textContent = imageDescription;
-    imageDescriptionContainer.classList.add('image-description');
-
-    const newLine = document.createElement('br');
-
-    const closeImageButton = document.createElement('button');
-    closeImageButton.classList.add('green-button');
-    closeImageButton.textContent="close";
-
-
-
-    closeImageButton.onclick = function () {
-      document.body.removeChild(fullscreenImage);
-    };
-  
-  
-    const image = document.createElement('img');
-    image.src = imageUrl;
-    image.style.maxWidth= '700px';
-    image.style.maxHeight='700px';
-    image.style.borderRadius='20px';
-
-    imageDescriptionContainer.appendChild(newLine);
-    imageDescriptionContainer.appendChild(closeImageButton);
-    fullscreenImage.appendChild(imageDescriptionContainer);
-    fullscreenImage.appendChild(image);
-    document.body.appendChild(fullscreenImage);
+  switch (i) {
+    case 0:
+      const imageDiv = document.createElement('div');
+      imageDiv.classList.add('gallery-column');
+      image.imageContainer = imageDiv;
+      imageDiv.appendChild(imageElement);
+      galleryContainer.appendChild(imageDiv);
+      break;
+    case 1:
+      const previousImgIndex = imagesArray.indexOf(image) - 1;
+      imagesArray[previousImgIndex].imageContainer.appendChild(imageElement);
+      // galleryContainer.appendChild(imagesArray[previousImgIndex].imageContainer);
+      break;
+    default:
+      console.log("Error with indexing");
+      break;
   }
+  i++;
+  if (i === 2) i = 0;
+}
+
+
+function showfullpicture(img) {
+
+  imageSrc = img.imgSource;
+  imageDescription = img.description;
+
+  
+  imageUrl = "Gallery/" + imageSrc;
+
+  const fullscreenImage = document.createElement('div');
+  fullscreenImage.classList.add('full-screen-image');
+
+  //IMAGE DESCRIPTION CONTAINER
+  const imageDescriptionContainer = document.createElement('div');
+  imageDescriptionContainer.textContent = imageDescription;
+  imageDescriptionContainer.classList.add('image-description');
+
+  // const newLine = document.createElement('br');
+
+  //BUTTON TO CLOSE IMAGE
+  const closeImageButton = document.createElement('button');
+  closeImageButton.classList.add('red-button');
+  closeImageButton.textContent = "close";
+  closeImageButton.addEventListener('click',function(){
+    document.body.removeChild(fullscreenImage);
+  });
+ 
+
+  //IMAGE ELEMENT
+  const image = document.createElement('img');
+  image.src = imageUrl;
+  image.style.maxWidth = '700px';
+  image.style.maxHeight = '700px';
+  image.style.borderRadius = '20px';
+  image
+
+  //BUTTON FOR NEXT PICTURE
+  const nextPictureButton = document.createElement('button');
+  nextPictureButton.classList.add('green-button');
+  nextPictureButton.textContent = "next";
+  nextPictureButton.addEventListener('click', function(){
+    document.body.removeChild(fullscreenImage);
+    const nextPictureIndex = imagesArray.indexOf(img)+1;
+    let nextPicture;
+    if(nextPictureIndex===imagesArray.length-1){
+      nextPicture = imagesArray[0];
+    }else{
+      nextPicture = imagesArray[nextPictureIndex];
+    }
+    showfullpicture(nextPicture);
+  });
+
+
+  //BUTTON FOR PREVIOUS PICTURE
+  const previousPictureButton = document.createElement('button');
+  previousPictureButton.classList.add('green-button');
+  previousPictureButton.textContent = "previous";
+  previousPictureButton.addEventListener('click', function(){
+    document.body.removeChild(fullscreenImage);
+    const previousPictureIndex = imagesArray.indexOf(img)-1;
+    let previousPicture;
+    if(previousPictureIndex===-1){
+      previousPicture = imagesArray[imagesArray.length-1];
+    }else{
+      previousPicture = imagesArray[previousPictureIndex];
+    }
+    showfullpicture(previousPicture);
+  });
+ 
+
+  imageDescriptionContainer.appendChild(document.createElement('br'));
+  imageDescriptionContainer.appendChild(previousPictureButton);
+  imageDescriptionContainer.appendChild(nextPictureButton);
+  imageDescriptionContainer.appendChild(document.createElement('br'));
+  imageDescriptionContainer.appendChild(document.createElement('br'));
+  imageDescriptionContainer.appendChild(closeImageButton);
+  fullscreenImage.appendChild(imageDescriptionContainer);
+  fullscreenImage.appendChild(image);
+  document.body.appendChild(fullscreenImage);
+}
